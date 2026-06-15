dc97baa043065cf7a3e1b7f79ed8f096765a20cb
Om Gupta <omgupta2473@gmail.com>
2026-06-15 15:04:38 +0530

    Add auth and users module foundation

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -9,15 +9,20 @@ import { AppService } from './app.service';
 
 import { PrismaModule } from './prisma/prisma.module';
 
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [configuration],
-      validationSchema: envValidationSchema,
-    }),
-    PrismaModule,
-  ],
+  ConfigModule.forRoot({
+    isGlobal: true,
+    load: [configuration],
+    validationSchema: envValidationSchema,
+  }),
+  PrismaModule,
+  UsersModule,
+  AuthModule,
+],
   controllers: [AppController],
   providers: [AppService],
 })
